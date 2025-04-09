bc9629f89699ff64569ca144b6f9d30afa3071b6
hypertonika <160002060+hypertonika@users.noreply.github.com>
2025-04-09 20:40:15 +0500

    apichange

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
 import asyncio
 from aiogram import Bot, Dispatcher
 from aiogram.fsm.storage.memory import MemoryStorage
-from app.testhandle import router 
+from app.testhandle import router
 import os
 
 
-
 async def main():
-    bot = Bot(token='7711483490:AAH1N1xXbMS5qXblhZgbv493M0Vdd77MubI')
+    bot = Bot(token="7440735369:AAFQBj9uRIBNjj4mhZ96_HyY8RQrljWwc1M")
     dp = Dispatcher(storage=MemoryStorage())
     dp.include_router(router)
     await dp.start_polling(bot)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     try:
         asyncio.run(main())
     except KeyboardInterrupt:
-        print('Bot is offline')
+        print("Bot is offline")
